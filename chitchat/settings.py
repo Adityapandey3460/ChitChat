55c0f4e3004e7805dfc601ad56b34bd317813287
Adityapandey3460 <adityapandey3460@gmail.com>
2025-09-13 23:00:46 +0530

    debug = false

--- chitchat/settings.py
+++ chitchat/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p=)s6xyy(y(%s4&f@mo01l6s31$c)5tf0*v+4vte52!0m%a3-('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS=["*"
 
